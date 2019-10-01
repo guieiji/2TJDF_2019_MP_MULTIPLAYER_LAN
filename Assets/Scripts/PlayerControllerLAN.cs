2500465c6223d5f22017a1247959c36838895071
Eiji <logonaluno@local.com>
2019-10-01 12:04:41 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerControllerLAN.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControllerLAN : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isLocalPlayer)
+        {
+            var h = Input.GetAxis("Horizontal");
+
+            transform.Translate(Vector3.right * (h * Time.deltaTime));
+        }
+    }
+}
